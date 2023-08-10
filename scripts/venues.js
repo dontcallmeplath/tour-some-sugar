3789b465de64b701b8e640903fdcfa5f454ee3df
Sylvia T <sylviarosemarie@gmail.com>
2023-08-10 14:14:48 -0500

    made click event to display bands playing the venue. has some wonky display issues bc of the randomizer. letting it slide for now.

--- scripts/venues.js
+++ scripts/venues.js
@@ -1,15 +1,44 @@
-import { getVenues } from "./database.js";
+import { getVenues, getBands } from "./database.js";
+import { dataRandomizer } from "./randomizer.js";
 const venues = getVenues();
+const bands = getBands();
+const bandArray = [];
 
 export const showVenues = () => {
-  let venueHTML = "<ul>";
+  let venueHTML = `<ul id="venues">`;
   venues.forEach((venue) => {
-    venueHTML += `<li>${venue.name}</li>`;
+    venueHTML += `<li data-type="venue" data-id="${dataRandomizer()}">${
+      venue.name
+    }</li>`;
   });
   venueHTML += "</ul>";
   return venueHTML;
 };
 
-// venues.forEach((venue) => {
-//   console.log(venue.bandid);
-// });
+const makeArray = () => {
+  for (const band of bands) {
+    bandArray.push(band.venueid);
+  }
+};
+makeArray();
+
+document.addEventListener("click", (clickEvent) => {
+  const clickedItem = clickEvent.target;
+  const item = clickedItem.dataset;
+  const id = parseInt(item.id);
+  let string = `The following bands are playing this venue: `;
+
+  if (item.type === "venue") {
+    bandArray.forEach((num) => {
+      if (num === id) {
+        for (const band of bands) {
+          if (id === band.id) {
+            string += `${band.name} `;
+          }
+        }
+        return string;
+      }
+    });
+  }
+  clickedItem.innerHTML = string;
+});
